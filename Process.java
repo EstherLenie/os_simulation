f5d5c8f2cc4e4bbc3184389eceed1ec270f73e6d
unknown <yveltamours@gmail.com>
2023-08-23 22:42:20 -0400

    process manager

--- Process.java
+++ Process.java
@@ -1,9 +1,35 @@
 public class Process{
-    public final int id;
-    public final string name;
+    private final int id;
+    private final String name;
+    private int execTime;
+    private boolean temporary;
 
-    public Process(id int, name string){
+    public Process(int id, String name, int execTime){
         this.id=id;
         this.name=name;
+        this.execTime=execTime;
+        this.temporary = true;
+    }
+
+    public Process(int id, String name){
+        this.id=id;
+        this.name=name;
+        this.execTime=10000;
+        this.temporary = false;
+    }
+
+    public int getPID(){
+        return this.id;
+    }
+
+    public void decreaseExecTime(int time){
+        if (!this.temporary){
+            return;
+        }
+        this.execTime -= time;
+    }
+
+    public boolean isFinished(){
+        return this.execTime <= 0;
     }
 }
